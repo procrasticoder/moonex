b14a345e01e83440ba60e0d38e4f2250cc50e7b4
procrasticoder <iamankitmishra07@gmail.com>
2021-12-30 02:25:03 +0530

    initial commit

---
+++ js/base.js
@@ -0,0 +1,156 @@
+document.getElementsByTagName('header')[0].innerHTML=`
+<nav class="navbar navbar-main navbar-expand-lg navbar-light">
+        <div class="carousel-detail-main">
+          <div class="carousel-detail-division">
+            <i class="fa fa-envelope px-3" style="color: #fff;"></i>
+            <a href="" style="text-decoration: none; color: #fff;"><b>moonextechnologies@gmail.com</b></a>
+          </div>
+          <div class="carousel-detail-division vLine">
+            <i class="fa fa-phone px-3" style="color: #fff;"></i>
+            <p style="color: #fff;">Call Us now on <b>+91-9825317640</b></p>
+          </div>
+          <div class="carousel-detail-division">
+            <a href="">
+              <i class="fa fa-facebook px-3" style="color: #fff;"></i>
+            </a><a href="">
+              <i class="fa fa-instagram px-3" style="color: #fff;"></i>
+            </a><a href="">
+              <i class="fa fa-youtube px-3" style="color: #fff;"></i>
+            </a>
+          </div>
+        </div>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">
+                <img src="photos/logo.png" alt="" width="300" height="50">
+              </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li id="home" class="nav-item mx-2">
+                <a class="nav-link" aria-current="page" href="index.html">Home</a>
+              </li>
+              <li id="about" class="nav-item mx-2">
+                <a class="nav-link" href="about_us.html">About Us</a>
+              </li>
+              <li id="products" class="nav-item dropdown mx-2">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Products
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li id="products"><a class="dropdown-item" href="products.html">JIS-Test-Powder</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li id="products"><a class="dropdown-item" href="products.html#nv2">Test-Dust</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li id="products"><a class="dropdown-item" href="products.html#nv3">SAP-Test-Powder</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li id="products"><a class="dropdown-item" href="products.html#nv4">Fuel-Contaminants</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li id="products"><a class="dropdown-item" href="products.html#nv5">Lab-Grade-Chemicals</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li id="products"><a class="dropdown-item" href="products.html">All Products</a></li>                  
+                </ul>
+              </li>              
+              <li id="contact" class="nav-item mx-2">
+                <a class="nav-link" href="contact.html">Contact</a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+    </nav>
+`;
+
+document.getElementsByTagName('footer')[0].innerHTML=`<div class="footer_one">
+<div class="q_links">
+    <h3>
+        Quick Links
+    </h3>
+    <a href="index.html">Home</a>
+    <a href="about_us.html">About Us</a>
+    <a href="products.html">Products</a>
+    <a href="contact.html">Contact</a>
+    <div class="footerline"></div>
+</div>
+
+<div class="q_links">
+    <h3>
+        Our Products
+    </h3>
+    <a  href="products.html">JIS Test Powder</a>
+          
+          <a  href="products.html#nv2">Test Dust</a>
+          
+          <a href="products.html#nv3">SAP Test Powder</a>
+        
+          <a href="products.html#nv4">Fuel Contaminants</a>
+          
+          <a href="products.html#nv5">Lab Grade Chemicals</a>
+          
+          <a href="products.html#nv6">Industrial Chemicals & Lubricant Oils</a>
+          
+          <a href="products.html#nv7">Rubber Processing Oil & Chemicals</a>
+
+          <a href="products.html#nv8">Food Grade Chemicals</a>
+    
+    <div class="footerline"></div>
+</div>
+
+<div class="q_contact">
+    <h3>
+        Contact Us
+    </h3>
+    <div class="q_contact_link">
+        <i class="fa fa-home"></i>
+        <p>Shop No, 800, near Lokdeep Public School
+          Mohna Road, Ballabhgarh, Faridabad, Haryana 121004</p>
+    </div><br>
+
+    <div class="q_contact_link">
+        <i class="fa fa-phone"></i>
+        <p>+91-9825317640</p>
+    </div><br>
+    <div class="q_contact_link">
+        <i class="fa fa-envelope"></i>
+        <a href="https://www.gmail.com" style="text-decoration: underline; cursor: pointer;">moonextechnologies@gmail.com</a>
+    </div><br>
+    <div class="footerline"></div>
+
+</div>
+
+<div class="q_follow_us">
+    <h3>
+        Follow Us
+    </h3>
+    <div class="q_follow_us_link">
+        <i class="fa fa-facebook"></i>
+        <p>facebook</p>
+    </div><br>
+
+    <div class="q_follow_us_link">
+        <i class="fa fa-instagram"></i>
+        <p>Instagram</p>
+    </div><br>
+    <div class="q_follow_us_link">
+        <i class="fa fa-youtube"></i>
+        <p>YouTube</p>
+    </div><br>
+    <div class="q_follow_us_link">
+        <i class="fa fa-twitter"></i>
+        <p>Twitter</p>
+    </div><br>
+    <div class="footerline"></div>
+
+</div>
+</div>
+<div class="footer_two">
+<div class="footer_content">
+    All Rights Reserved. Moonex Technologies (Terms of Use)<br>
+                    <a href="https://adjointindia.com/" style="color: inherit; cursor: pointer;">Powered by AdJoint India</a>
+</div>
+<div class="footer_logo">
+    <img style="cursor:pointer;cursor:hand" src="https://seal.starfieldtech.com/images/3/en/siteseal_sf_3_h_l_m.gif"  alt="SSL site seal - click to verify">
+</div>
+</div>`;
+
